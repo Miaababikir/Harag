ddd79d1a2cab323bede502e8d116e61c3ec29eef
Miaababikir <miaababikir@gmail.com>
2019-03-20 21:31:27 +0200

    Adding social login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Traits\AuthenticatesAdmins;
+use App\User;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
+use Laravel\Socialite\Facades\Socialite;
 
 class LoginController extends Controller
 {
@@ -37,4 +40,47 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    /**
+     * Redirect the user to the GitHub authentication page.
+     *
+     * @param $provider
+     * @return \Illuminate\Http\Response
+     */
+    public function redirectToProvider($provider)
+    {
+        return Socialite::driver($provider)->redirect();
+    }
+
+    /**
+     * Obtain the user information from GitHub.
+     *
+     * @param $provider
+     * @return \Illuminate\Http\Response
+     */
+    public function handleProviderCallback($provider)
+    {
+        /*
+         * TODO Refactoring, it's not clean yet
+         * Maybe it crash when we start using others providers
+         */
+
+        $socialUser = Socialite::driver($provider)->user();
+
+        $user = User::firstOrCreate([
+            'email' => $socialUser->getEmail(),
+            'fullname' => $socialUser->getName(),
+            'username' => $socialUser->getNickname(),
+            'slug' => Str::slug($socialUser->getNickname(), '-'),
+            'provider_id' => $socialUser->getId(),
+            'provider' => $provider,
+        ]);
+
+        Auth::login($user);
+
+        $user->sendEmailVerificationNotification();
+
+        return redirect($this->redirectTo);
+    }
+
 }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,85 +2,88 @@
 
 @section('content')
 
-<section class="chart-page login gray-bg padding-top-100 padding-bottom-100">
-    <div class="container">
+    <section class="chart-page login gray-bg padding-top-100 padding-bottom-100">
+        <div class="container">
 
-        <!-- Payments Steps -->
-        <div class="shopping-cart">
+            <!-- Payments Steps -->
+            <div class="shopping-cart">
 
-            <!-- SHOPPING INFORMATION -->
-            <div class="cart-ship-info">
-                <div class="row">
+                <!-- SHOPPING INFORMATION -->
+                <div class="cart-ship-info">
+                    <div class="row">
 
-                    <!-- Login Register -->
-                    <div class="col-sm-7 center-block">
+                        <!-- Login Register -->
+                        <div class="col-sm-7 center-block">
 
-                        <!-- Nav Tabs -->
-                        <ul class="nav">
-                            <li class="nav-item">
-                                <a class="nav-link active">Login</a>
-                            </li>
-                        </ul>
+                            <!-- Nav Tabs -->
+                            <ul class="nav">
+                                <li class="nav-item">
+                                    <a class="nav-link active">Login</a>
+                                </li>
+                            </ul>
 
-                        <!-- Login Register Inside -->
-                        <div class="tab-content" id="myTabContent">
+                            <!-- Login Register Inside -->
+                            <div class="tab-content" id="myTabContent">
 
-                            <!-- Login -->
-                            <div class="tab-pane fade show active" id="log" role="tabpanel" aria-labelledby="login-tab">
-                                <form method="POST" action="{{ route('login') }}">
-                                    @csrf
-                                    <ul class="row">
+                                <!-- Login -->
+                                <div class="tab-pane fade show active" id="log" role="tabpanel"
+                                     aria-labelledby="login-tab">
+                                    <form method="POST" action="{{ route('login') }}">
+                                        @csrf
+                                        <ul class="row">
 
-                                        <!-- Email -->
-                                        <li class="col-md-12">
-                                            <label>Email Address
-                                                <input type="text" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}"
-                                                    name="email" value="{{ old('email') }}" required autofocus>
-                                                @if ($errors->has('email'))
-                                                <span class="invalid-feedback" role="alert">
+                                            <!-- Email -->
+                                            <li class="col-md-12">
+                                                <label>Email Address
+                                                    <input type="text"
+                                                           class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}"
+                                                           name="email" value="{{ old('email') }}" required autofocus>
+                                                    @if ($errors->has('email'))
+                                                        <span class="invalid-feedback" role="alert">
                                                     <strong>{{ $errors->first('email') }}</strong>
                                                 </span>
-                                                @endif
-                                            </label>
-                                        </li>
-                                        <!-- Password -->
-                                        <li class="col-md-12">
-                                            <label>Password
-                                                <input type="password" name="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}"
-                                                    required>
+                                                    @endif
+                                                </label>
+                                            </li>
+                                            <!-- Password -->
+                                            <li class="col-md-12">
+                                                <label>Password
+                                                    <input type="password" name="password"
+                                                           class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}"
+                                                           required>
 
-                                                @if ($errors->has('password'))
-                                                <span class="invalid-feedback" role="alert">
+                                                    @if ($errors->has('password'))
+                                                        <span class="invalid-feedback" role="alert">
                                                     <strong>{{ $errors->first('password') }}</strong>
                                                 </span>
-                                                @endif
-                                            </label>
-                                        </li>
+                                                    @endif
+                                                </label>
+                                            </li>
 
-                                        <!-- LOGIN -->
-                                        <li class="col-md-6">
-                                            <button type="submit" class="btn">LOGIN</button>
-                                        </li>
+                                            <!-- LOGIN -->
+                                            <li class="col-md-6">
+                                                <button type="submit" class="btn">LOGIN</button>
+                                            </li>
 
-                                        <!-- FORGET PASS -->
-                                        <li class="col-md-6">
-                                            <div class="margin-top-15 text-right"> <a href="{{route('register')}}">Register</a>
-                                            </div>
-                                        </li>
+                                            <!-- FORGET PASS -->
+                                            <li class="col-md-6">
+                                                <div class="margin-top-15 text-right"><a href="{{route('register')}}">Register</a>
+                                                </div>
+                                            </li>
+                                        </ul>
+                                    </form>
+
+                                    <!-- Main Heading -->
+                                    <div class="heading text-center margin-bottom-50 margin-top-50">
+                                        <h4>LOGIN WITH SOCIAL MEDIA</h4>
+                                        <hr>
+                                    </div>
+                                    <ul class="login-with">
+                                        <li><a href="#"><i class="fa fa-facebook"></i>FACEBOOK</a></li>
+                                        <li><a href="#"><i class="fa fa-google"></i>GOOGLE</a></li>
+                                        <li><a href="{{ route('login.social', 'github') }}"><i class="fa fa-github"></i>GITHUB</a></li>
                                     </ul>
-                                </form>
-
-                                <!-- Main Heading -->
-                                <div class="heading text-center margin-bottom-50 margin-top-50">
-                                    <h4>LOGIN WITH SOCIAL MEDIA</h4>
-                                    <hr>
                                 </div>
-                                <ul class="login-with">
-                                    <li> <a href="#."><i class="fa fa-facebook"></i>FACEBOOK</a> </li>
-                                    <li> <a href="#."><i class="fa fa-google"></i>GOOGLE</a> </li>
-                                    <li> <a href="#."><i class="fa fa-twitter"></i>TWITTER</a> </li>
-                                </ul>
-                            </div>
 
                             </div>
                         </div>
@@ -88,21 +91,18 @@
                 </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
 
 @endsection
 @section('scripts')
-<script>
-    const app = new Vue({
-        el: '#app',
-        data: {
-            search: '',
-        },
-        methods: {
+    <script>
+        const app = new Vue({
+            el: '#app',
+            data: {
+                search: '',
+            },
+            methods: {},
+        });
 
-        },
-    });
-
-</script>
+    </script>
 @endsection
